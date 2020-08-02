aef4ed5bee7cc6099757e39425b09cd2fe26f7a8
nikaku <qurdadzenika@gmail.com>
2020-08-02 19:07:12 +0400

    Add project files.

---
+++ NoSenseTask/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoSenseTask
+{
+    public static class Extensions
+    {
+        public static T ThisDoesntMakeAnySense<T>(this IEnumerable<T> data, Func<T, bool> predicate, Func<T> Creator)
+        {
+            foreach (T value in data)
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (predicate(value))
+                {
+                    return value;
+                }
+            }
+            return Creator();
+        }
+
+
+    }
+}
---
+++ NoSenseTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NoSenseTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntts;
+
+            while (true)
+            {
+                Console.WriteLine("Please, input an array of integers");
+                try
+                {
+                    var userInput = Console.ReadLine(); //"12, 123, 14, 23, 17, 16" 
+                    if (string.IsNullOrWhiteSpace(userInput))
+                    {
+                        throw new ArgumentNullException("Initial Array is null");
+                    }
+                    arrayOfIntts = userInput.Split(",").Select(x => Convert.ToInt32(x)).ToArray();
+                    object result1 = arrayOfIntts.ThisDoesntMakeAnySense(x => x > 14 && x < 100, () => 1);
+                    object result2 = arrayOfIntts.ThisDoesntMakeAnySense(x => x == 1000, () => 1);
+
+                    Console.WriteLine($"Input : {userInput} - Result1  : {result1}  - Result2  : {result2}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}; Input is not a valid array of integers");
+                    continue;
+                }
+            }
+
+
+        }
+    }
+}
